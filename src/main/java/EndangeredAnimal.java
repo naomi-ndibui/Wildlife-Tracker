43a86e0c33ea18e1fc17972dddf63c10d5bc1311
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-28 16:18:12 +0300

    added file declared variables from animal class

---
+++ src/main/java/EndangeredAnimal.java
@@ -0,0 +1,14 @@
+public class EndangeredAnimal {
+    private  String health;
+    private  String age;
+
+    public static final String HEALTH_HEALTHY = "healthy";
+    public static final String HEALTH_ILL = "ill";
+    public static final String HEALTH_OKAY = "okay";
+
+    public static final String AGE_NEWBORN = "newborn";
+    public static final String AGE_YOUNG = "young";
+    public static final String AGE_ADULT = "adult";
+
+
+}

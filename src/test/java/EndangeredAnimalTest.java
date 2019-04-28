ffb4bbb1b1a98d906040f362f00ad5c99fb838c0
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-28 16:32:42 +0300

    added sighting and tests

--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -12,5 +12,90 @@ public class EndangeredAnimalTest {
         EndangeredAnimal testEndangeredAnimal = endangered1;
         assertEquals(true, testEndangeredAnimal instanceof EndangeredAnimal);
     }
+    @Test
+    public void getName_animalInstantiatesWithName_koala() {
+        EndangeredAnimal testEndangeredAnimal = endangered1;
+        assertEquals("Spotted koala", testEndangeredAnimal.getName());
+    }
+    @Test
+    public void getEndangered_animalInstantiatesWithEndangered_no() {
+        EndangeredAnimal testEndangeredAnimal = endangered1;
+        assertEquals("yes", testEndangeredAnimal.getEndangered());
+    }
+    @Test
+    public void getEndangered_personInstantiatesWithAge_young() {
+        EndangeredAnimal testEndangeredAnimal = endangered1;
+        assertEquals("young", testEndangeredAnimal.getAge());
+    }
 
+    @Test
+    public void getEndangered_personInstantiatesWithHealth_ill() {
+        EndangeredAnimal testEndangeredAnimal = endangered1;
+        assertEquals("ill", testEndangeredAnimal.getHealth());
+    }
+
+    @Test
+    public void equals_returnsTrueIfNameAreSame_true() {
+        EndangeredAnimal testEndangeredAnimal = endangered1;
+        EndangeredAnimal anotherEndangeredAnimal = endangered1;
+        assertTrue(testEndangeredAnimal.equals(anotherEndangeredAnimal));
+    }
+
+    @Test
+    public void save_insertsObjectIntoDatabase_EndangeredAnimal() {
+        EndangeredAnimal testEndangeredAnimal = endangered1;
+        testEndangeredAnimal.save();
+        assertEquals(true, Animal.all().get(0).equals(testEndangeredAnimal));
+    }
+
+    @Test
+    public void save_assignsIdToEndangeredAnimal() {
+        EndangeredAnimal testEndangeredAnimal = endangered1;
+        testEndangeredAnimal.save();
+        Animal savedEndangeredAnimal = Animal.all().get(0);
+        assertEquals(savedEndangeredAnimal.getId(), testEndangeredAnimal.getId());
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfEndangeredAnimal_true() {
+        EndangeredAnimal firstEndangeredAnimal = endangered1;
+        firstEndangeredAnimal.save();
+        EndangeredAnimal secondEndangeredAnimal = endangered2;
+        secondEndangeredAnimal.save();
+        assertEquals(true, Animal.all().get(0).equals(firstEndangeredAnimal));
+        assertEquals(true, Animal.all().get(1).equals(secondEndangeredAnimal));
+    }
+
+    @Test
+    public void find_returnsEndangeredAnimalWithSameId_secondEndangeredAnimal() {
+        EndangeredAnimal firstEndangeredAnimal = endangered1;
+        firstEndangeredAnimal.save();
+        EndangeredAnimal secondEndangeredAnimal = endangered2;
+        secondEndangeredAnimal.save();
+        assertEquals(Animal.find(secondEndangeredAnimal.getId()), secondEndangeredAnimal);
+    }
+
+    @Test
+    public void setEndangered_Animal_true() {
+        EndangeredAnimal myEndangeredAnimal = new EndangeredAnimal("Spotted koala", "no", "healthy", "young");
+        myEndangeredAnimal.save();
+        myEndangeredAnimal.setEndangered("yes", "ill", "newborn");
+        assertEquals("yes", EndangeredAnimal.find(myEndangeredAnimal.getId()).getEndangered());
+        assertEquals("ill", EndangeredAnimal.find(myEndangeredAnimal.getId()).getHealth());
+        assertEquals("newborn", EndangeredAnimal.find(myEndangeredAnimal.getId()).getAge());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setEndangered_throwsExceptionIfHealthIsNotAChoice(){
+        EndangeredAnimal myEndangeredAnimal = new EndangeredAnimal("Spotted koala", "no", "healthy", "young");
+        myEndangeredAnimal.save();
+        myEndangeredAnimal.setEndangered("yes", "blech", "newborn");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setEndangered_throwsExceptionIfAgeIsNotAChoice(){
+        EndangeredAnimal myEndangeredAnimal = new EndangeredAnimal("Spotted koala", "no", "healthy", "young");
+        myEndangeredAnimal.save();
+        myEndangeredAnimal.setEndangered("yes", "ill", "blech");
+    }
 }
---
+++ src/test/java/SightingTest.java
@@ -0,0 +1,89 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class SightingTest {
+
+    Sighting sighting1 = new Sighting("Henry", "Zone B", 1);
+    Sighting sighting2 = new Sighting("Henrietta", "Zone A", 2);
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void sighting_instantiatesCorrectly_true() {
+        Sighting testSighting = sighting1;
+        assertEquals(true, testSighting instanceof Sighting);
+    }
+
+    @Test
+    public void getRangerName_personInstantiatesWithRangerName_Henry() {
+        Sighting testSighting = sighting1;
+        assertEquals("Henry", testSighting.getRangerName());
+    }
+
+    @Test
+    public void getLocation_personInstantiatesWithLocation_ZoneB() {
+        Sighting testSighting = sighting1;
+        assertEquals("Zone B", testSighting.getLocation());
+    }
+
+    @Test
+    public void getAnimalId_personInstantiatesWithAnimalId_1() {
+        Sighting testSighting = sighting1;
+        assertEquals(1, testSighting.getAnimalId());
+    }
+
+    @Test
+    public void equals_returnsTrueIfNameAndLocationAreSame_true() {
+        Sighting testSighting = sighting1;
+        Sighting anotherSighting = sighting1;
+        assertTrue(testSighting.equals(anotherSighting));
+    }
+
+    @Test
+    public void save_insertsObjectIntoDatabase_Sighting() {
+        Sighting testSighting = sighting1;
+        testSighting.save();
+        assertEquals(true, Sighting.all().get(0).equals(testSighting));
+    }
+
+    @Test
+    public void save_assignsIdToSighting() {
+        Sighting testSighting = sighting1;
+        testSighting.save();
+        Sighting savedSighting = Sighting.all().get(0);
+        assertEquals(savedSighting.getId(), testSighting.getId());
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfSighting_true() {
+        Sighting firstSighting = sighting1;
+        firstSighting.save();
+        Sighting secondSighting = sighting2;
+        secondSighting.save();
+        assertEquals(true, Sighting.all().get(0).equals(firstSighting));
+        assertEquals(true, Sighting.all().get(1).equals(secondSighting));
+    }
+
+    @Test
+    public void find_returnsSightingWithSameId_secondSighting() {
+        Sighting firstSighting = sighting1;
+        firstSighting.save();
+        Sighting secondSighting = sighting2;
+        secondSighting.save();
+        assertEquals(Sighting.find(secondSighting.getId()), secondSighting);
+    }
+
+    @Test
+    public void save_recordsTimeOfCreationInDatabase() {
+        Sighting testSighting = sighting1;
+        testSighting.save();
+        Timestamp savedSightingTime = Sighting.find(testSighting.getId()).getTimestamp();
+        Timestamp rightNow = new Timestamp(new Date().getTime());
+        assertEquals(rightNow.getDay(), savedSightingTime.getDay());
+    }
+
+}

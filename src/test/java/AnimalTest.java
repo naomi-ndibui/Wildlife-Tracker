3fd899a4e0252570884066c8a67c021a0789b364
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-28 13:49:21 +0300

    created a test to instantiate true then added a animal constructor

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,2 +1,11 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
 public class AnimalTest {
+    @Test
+    public void animal_instantiatesCorrectly_true() {
+        Animal testAnimal = animal1;
+        assertEquals(true, testAnimal instanceof Animal);
+    }
 }

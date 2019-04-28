a0de5ebd765b2632da51337f65ac19e9aab4aa8f
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-28 16:59:55 +0300

    added static file location and base file and imported spark

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,8 @@
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+    }
 }

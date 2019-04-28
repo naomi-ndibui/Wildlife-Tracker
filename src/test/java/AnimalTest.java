f9334a616569e9f3edc67b910d1ea263899cd8fb
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-28 15:42:25 +0300

    added a rule

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -3,6 +3,10 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+    
     @Test
     public void animal_instantiatesCorrectly_true() {
         Animal testAnimal = animal1;

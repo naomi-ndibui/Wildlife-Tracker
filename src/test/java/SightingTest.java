bdabc9213a8206f7831cd9acb8180cfd5ac8bb81
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-30 10:33:46 +0300

    commit

--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -9,42 +9,44 @@ public class SightingTest {
     Sighting sighting1 = new Sighting("Henry", "Zone B", 1);
     Sighting sighting2 = new Sighting("Henrietta", "Zone A", 2);
 
+    Sighting testSighting = sighting1;
+
     @Rule
     public DatabaseRule database = new DatabaseRule();
 
     @Test
     public void sighting_instantiatesCorrectly_true() {
-        Sighting testSighting = sighting1;
+//        Sighting testSighting = sighting1;
         assertEquals(true, testSighting instanceof Sighting);
     }
 
     @Test
     public void getRangerName_personInstantiatesWithRangerName_Henry() {
-        Sighting testSighting = sighting1;
+//        Sighting testSighting = sighting1;
         assertEquals("Henry", testSighting.getRangerName());
     }
 
     @Test
     public void getLocation_personInstantiatesWithLocation_ZoneB() {
-        Sighting testSighting = sighting1;
+//        Sighting testSighting = sighting1;
         assertEquals("Zone B", testSighting.getLocation());
     }
 
     @Test
     public void getAnimalId_personInstantiatesWithAnimalId_1() {
-        Sighting testSighting = sighting1;
+//        Sighting testSighting = sighting1;
         assertEquals(1, testSighting.getAnimalId());
     }
 
     @Test
     public void getSightedAnimalName_returnsAnimalName_koala() {
-        Sighting testSighting = sighting1;
+//        Sighting testSighting = sighting1;
         assertEquals("koala", testSighting.getSightedAnimalName());
     }
 
     @Test
     public void getSightedAnimalEndangeredStatus_returnsAnimalEndangeredStatus_No() {
-        Sighting testSighting = sighting1;
+//        Sighting testSighting = sighting1;
         assertEquals("No", testSighting.getSightedAnimalEndangeredStatus());
     }
 

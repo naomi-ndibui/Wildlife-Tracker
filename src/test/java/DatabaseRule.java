7e4d50bffd7a1afb1698efb0809275eb88664f41
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-30 08:12:37 +0300

    changed the first characters of database name to lowercase

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,6 +5,6 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/Wildlife_Tracker_test", "ndibui", "qwerty");
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "ndibui", "qwerty");
     }
 }
